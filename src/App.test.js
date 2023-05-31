92403da08f5d07043167dd68a9164a2fb86b3c38
Gaurav Sharma <gsharma.nus@gmail.com>
2023-06-01 00:59:47 +0800

    📦Install essential packages

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { App } from './App';
 
-test('renders learn react link', () => {
+it('renders simple webpage with greeting', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/Hi there/i)).toBeInTheDocument();
 });
